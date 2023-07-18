486c60d4fdac929244a1666548ccc61f8d70c126
Ilya Novik <iinovik@edu.hse.ru>
2023-07-18 20:12:24 +0300

    topbar elevate

--- app/src/main/java/com/ilyanvk/todoapp/ui/todoeditor/compose/TodoEditorScreen.kt
+++ app/src/main/java/com/ilyanvk/todoapp/ui/todoeditor/compose/TodoEditorScreen.kt
@@ -1,13 +1,18 @@
 package com.ilyanvk.todoapp.ui.todoeditor.compose
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,27 +37,39 @@ fun TodoEditorScreen(
     val priority = todoItem.priority
     val deadline = todoItem.deadline
 
+    val scrollState = rememberScrollState()
+    val elevation by animateDpAsState(
+        targetValue = if (scrollState.value > 0) 4.dp else 0.dp, label = "elevationAnimation"
+    )
+
+
     Scaffold(
-        topBar = { TodoEditorTopBar(text = text, onAction = onAction) },
-        containerColor = AppTheme.colors.backPrimary
-    ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            item {
-                TodoEditorTextField(text = text, onAction = onAction)
-                TodoEditorPriorityField(priority = priority, onAction = onAction)
-                TodoEditorDivider(PaddingValues(horizontal = 16.dp))
-                TodoEditorDeadlineField(deadline = deadline, onAction = onAction)
-                TodoEditorDivider(PaddingValues(top = 24.dp, bottom = 8.dp))
-                TodoEditorDeleteField(
-                    enabled = isEditing || todoItem.text.isNotEmpty(),
+        topBar = {
+            Surface(shadowElevation = elevation) {
+                TodoEditorTopBar(
+                    text = text,
                     onAction = onAction
                 )
-                Spacer(Modifier.size(33.dp))
             }
+        },
+        containerColor = AppTheme.colors.backPrimary
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(state = scrollState)
+                .padding(paddingValues)
+        ) {
+            TodoEditorTextField(text = text, onAction = onAction)
+            TodoEditorPriorityField(priority = priority, onAction = onAction)
+            TodoEditorDivider(PaddingValues(horizontal = 16.dp))
+            TodoEditorDeadlineField(deadline = deadline, onAction = onAction)
+            TodoEditorDivider(PaddingValues(top = 24.dp, bottom = 8.dp))
+            TodoEditorDeleteField(
+                enabled = isEditing || todoItem.text.isNotEmpty(),
+                onAction = onAction
+            )
+            Spacer(Modifier.size(33.dp))
         }
     }
 }
